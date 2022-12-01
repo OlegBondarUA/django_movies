026fdc7edaf85cff9bfb26a6c5b0d3ead12e9218
OlegBondarUA <petrovichtrh@gmail.com>
2022-12-01 12:58:32 +0200

    add tests

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from unittest import TestCase
 
-# Create your tests here.
+from django.test import Client
+
+
+class UserTestCase(TestCase):
+
+    def test_client(self):
+        client = Client()
+        response = client.post('/login/', {'username': 'john', 'password': 'smith'})
+        result = response.status_code
+        self.assertEqual(result, 200)
+
+    def test_content(self):
+        client = Client()
+        response = client.get('/login/')
+        result = response.content
+        self.assertEqual(type(result), bytes)
