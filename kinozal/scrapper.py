62676f59bcbf8f5f354818f49d2951cced75a6a9
OlegBondarUA <petrovichtrh@gmail.com>
2023-01-15 15:02:18 +0200

    edit _process method and add ,_extract_data_from_soup, _save_data_to_db method

--- kinozal/scrapper.py
+++ kinozal/scrapper.py
@@ -15,6 +15,7 @@ from kinozal.models import Category, Director, Actor, Film, Country
 
 
 logger = logging.getLogger('logit')
+translator = Translator(from_lang="uk", to_lang="en")
 
 
 class ScrapeMovies:
@@ -67,98 +68,121 @@ class ScrapeMovies:
 
     @atomic
     def _process(self, html_string: str, url):
-        translator = Translator(from_lang="uk", to_lang="en")
         soup = BeautifulSoup(html_string, 'html.parser')
-        with self.LOCK:
-            try:
-                title = soup.select(".page__header h1")
-                title = title[0].text.strip().split('(')[0]
-                title_slug = translator.translate(title).replace(' ', '-').lower()
+        data = self._extract_data_from_soup(soup, url)
+        self._save_data_to_db(data)
 
-                release_year = soup.select('.page__details ul li a')
-                release_year = release_year[0].text.strip()
+    def _extract_data_from_soup(self, soup, url):
 
-                country = soup.select('.page__details ul li span')
-                country = country[1].text.strip().split(',')
+        title = soup.select(".page__header h1")
+        title = title[0].text.strip().split('(')[0]
+        title_slug = translator.translate(title).replace(' ', '-').lower()
 
-                views = soup.select('.page__activity-item')
-                views = views[0].text.strip()
+        release_year = soup.select('.page__details ul li a')
+        release_year = release_year[0].text.strip()
 
-                rating = soup.select('.page__rating-item')
-                rating = rating[0].text.strip().split(' ')[1]
+        country = soup.select('.page__details ul li span')
+        country = country[1].text.strip().split(',')
 
-                description = soup.select('.page__text')
-                description = description[0].text.strip()
+        views = soup.select('.page__activity-item')
+        views = views[0].text.strip()
 
-                movie_link = soup.select('.tabs-block__content iframe')
-                movie_link = movie_link[1].get('src')
+        rating = soup.select('.page__rating-item')
+        rating = rating[0].text.strip().split(' ')[1]
 
-                trailer_link = soup.select('.page__trailer iframe')
-                trailer_link = trailer_link[0].get('src')
+        description = soup.select('.page__text')
+        description = description[0].text.strip()
 
-                category = soup.select('.page__meta-item')
-                category = category[0].text.strip().split('/')
+        movie_link = soup.select('.tabs-block__content iframe')
+        movie_link = movie_link[1].get('src')
 
-                directors = soup.select('.page__details-list li span')
-                directors = directors[5].text.strip().split(',')
+        trailer_link = soup.select('.page__trailer iframe')
+        trailer_link = trailer_link[0].get('src')
 
-                actors = soup.select('.page__info-subinfo .line-clamp')
-                actors = actors[0].text.strip().split(': ')[1:]
-                actors = actors[0].split(',')
+        category = soup.select('.page__meta-item')
+        category = category[0].text.strip().split('/')
 
-                image = soup.select('.page__poster img')
-                image = f'https://ilovekino.online' \
-                        f'{image[0].get("data-src").strip()}'
+        directors = soup.select('.page__details-list li span')
+        directors = directors[5].text.strip().split(',')
 
-                image_name = image.split('-')[-1].split('/')[0].replace(' ',
-                                                                        '-')
-                self._upload_image_to_local_media(image, image_name)
+        actors = soup.select('.page__info-subinfo .line-clamp')
+        actors = actors[0].text.strip().split(': ')[1:]
+        actors = actors[0].split(',')
 
-                film, create = Film.objects.get_or_create(
-                    defaults={
-                        'base_url': url,
-                        'title': title,
-                        'image': f'images/{image_name}',
-                        'release_year': release_year,
-                        'views': views,
-                        'rating': rating,
-                        'description': description,
-                        'movie_link': movie_link,
-                        'trailer_link': trailer_link,
-                    },
-                    slug=slugify(title_slug := title_slug)
-                )
+        image = soup.select('.page__poster img')
+        image = f'https://ilovekino.online' \
+                f'{image[0].get("data-src").strip()}'
 
-                for item in country:
-                    countries, _ = Country.objects.get_or_create(
-                        country=item.strip())
-                    film.country.add(countries)
+        image_name = image.split('-')[-1].split('/')[0].replace(' ',
+                                                                '-')
+        self._upload_image_to_local_media(image, image_name)
 
-                for categor in category:
-                    category_slag = translator.translate(categor)
-                    cat, create = Category.objects.get_or_create(
-                        name=categor.strip(),
-                        slug=slugify(category_slag := category_slag.strip().lower()))
-                    film.categories.add(cat)
-
-                for dire in directors:
-                    direct, _ = Director.objects.get_or_create(name=dire)
-                    film.directors.add(direct)
-
-                for actor in actors:
-                    act, _ = Actor.objects.get_or_create(name=actor)
-                    film.actors.add(act)
-
-            except Exception as error:
-                exc_type, exc_obj, exc_tb = sys.exc_info()
-                logger.error(f'Parser Error {error} {exc_tb.tb_lineno}')
+        self._upload_image_to_local_media(image, image_name)
+        data = {
+            'base_url': url,
+            'title': title,
+            'image': f'images/{image_name}',
+            'release_year': release_year,
+            'views': views,
+            'rating': rating,
+            'description': description,
+            'movie_link': movie_link,
+            'trailer_link': trailer_link,
+            'slug': slugify(title_slug),
+            'country': country,
+            'category': category,
+            'directors': directors,
+            'actors': actors,
+        }
+        return data
 
     def _upload_image_to_local_media(self, img_url: str, image_name: str):
         with requests.Session() as session:
             img_response = session.get(img_url, timeout=self.TIME)
 
-        with open(f'media/images/{image_name}', 'wb') as file:
-            file.write(img_response.content)
+        with open(f'media/images/{image_name}', 'wb') as img_file:
+            img_file.write(img_response.content)
+
+    @atomic
+    def _save_data_to_db(self, data):
+        try:
+            film, create = Film.objects.get_or_create(
+                defaults={
+                    'base_url': data['base_url'],
+                    'title': data['title'],
+                    'image': data['image'],
+                    'release_year': data['release_year'],
+                    'views': data['views'],
+                    'rating': data['rating'],
+                    'description': data['description'],
+                    'movie_link': data['movie_link'],
+                    'trailer_link': data['trailer_link'],
+                },
+                slug=data['slug']
+            )
+
+            for item in data['country']:
+                countries, _ = Country.objects.get_or_create(
+                    country=item.strip())
+                film.country.add(countries)
+
+            for categor in data['category']:
+                category_slag = translator.translate(categor)
+                cat, create = Category.objects.get_or_create(
+                    name=categor.strip(),
+                    slug=slugify(
+                        category_slag := category_slag.strip().lower()))
+                film.categories.add(cat)
+
+            for dire in data['directors']:
+                direct, _ = Director.objects.get_or_create(name=dire)
+                film.directors.add(direct)
+
+            for actor in data['actors']:
+                act, _ = Actor.objects.get_or_create(name=actor)
+                film.actors.add(act)
+        except Exception as e:
+            logger.error(f'Save to db error: {e}')
 
 
 def main():
@@ -166,7 +190,7 @@ def main():
     with open(f'{settings.BASE_DIR}/kinozal/links.txt') as file:
         links = file.readlines()
 
-    for url in links[500:510]:
+    for url in links[700:703]:
         list_url.append(url.strip())
 
     scraper = ScrapeMovies(list_url)
