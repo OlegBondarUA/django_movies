e353210f38b83f6ae7d21f59988bb1b0a74090a3
OlegBondarUA <petrovichtrh@gmail.com>
2023-01-15 14:19:27 +0200

    edit test

--- kinozal/tests/test_selectors.py
+++ kinozal/tests/test_selectors.py
@@ -13,7 +13,7 @@ from kinozal.models import Category, Film, Director
 
 
 class TestSelectors(TestCase):
-
+    # Preparation of textual data
     def setUp(self):
         self.user = get_user_model().objects.create(username='some_user')
         self.category1 = Category(name='comedy', slug='comedy-slug')
@@ -34,10 +34,10 @@ class TestSelectors(TestCase):
 
 
         self.film = Film(title='avatar',
-                         release_year= 2002,
+                         release_year=2002,
                          image='/Users/olegbondar/Python/project_django/Beetroot_django/media/images/255798820.jpg',
-                         views= 100,
-                         rating= 9,
+                         views=100,
+                         rating=9,
                          description="posting",
                          movie_link='https://www.youtube.com/watch?v=5PSNL1qE6VY',
                          trailer_link='https://www.youtube.com/watch?v=5PSNL1qE6VY',
@@ -91,42 +91,114 @@ class TestSelectors(TestCase):
         self.film.directors.add(self.director4, self.director1)
 
     def test_random_films_selector(self):
-        films = random_films_selector(2)
-        self.assertEqual(len(films), 2)
+        # Test with default films_number (4)
+        films = random_films_selector()
+        self.assertEqual(films.count(), 4)
+        self.assertEqual(len(films), len(set(films)))
+
+        # Test with films_number=2
+        films = random_films_selector(films_number=2)
+        self.assertEqual(films.count(), 2)
+        self.assertEqual(len(films), len(set(films)))
+
+        # Test with films_number > number of films in db
+        films = random_films_selector(films_number=10)
+        self.assertEqual(films.count(), 4)  # four films in the setUp
+        self.assertEqual(len(films), len(set(films)))
+
+        # Test with empty db
+        Film.objects.all().delete()
+        films = random_films_selector()
+        self.assertEqual(films.count(), 0)
+        self.assertEqual(len(films), len(set(films)))
 
     def test_max_rating_selector(self):
-        result = max_rating_selector(2, 3)
-        self.assertEqual(result[0].title, 'alone at home')
+        # Test with default films_number (4)
+        films = max_rating_selector()
+        self.assertEqual(films.count(), 4)
+        for film in films:
+            self.assertGreaterEqual(film.rating, films[3].rating)
+        # Test with films_number=2
+        films = max_rating_selector(1, 3)
+        self.assertEqual(films.count(), 2)
+        for film in films:
+            self.assertGreaterEqual(film.rating, films[1].rating)
+        # Test with films_number > number of films in db
+        films = max_rating_selector(0, 10)
+        self.assertEqual(films.count(), 4)  # four films in the setUp
+        for film in films:
+            self.assertGreaterEqual(film.rating, films[3].rating)
+        # Test with empty db
+        Film.objects.all().delete()
+        films = max_rating_selector()
+        self.assertEqual(films.count(), 0)
 
     def test_categories_selector(self):
-        result = categories_selector()
-        self.assertEqual(len(result), 3)
-        self.assertEqual(result[1].name , 'comedy')
+        # Test with default films_number (3)
+        films = categories_selector()
+        self.assertEqual(films.count(), 3)
+        self.assertEqual(films[0].slug, self.category2.slug)
+
+        # Test with empty db
+        Film.objects.all().delete()
+        films = categories_selector()
+        self.assertEqual(films.count(), 0)
 
     def test_years_selector(self):
-        result = years_selector()
-        self.assertEqual(len(result), 4)
-        self.assertEqual(result[2], 2000)
+        # Test with default films_number (4)
+        films = years_selector()
+        self.assertEqual(films.count(), 4)
+        self.assertEqual(films[3], 1992)
+        # Test with films_number=2
+        films = years_selector()[0:2]
+        self.assertEqual(films.count(), 2)
+        self.assertEqual(films[1], 2002)
+        # Test with films_number > number of films in db
+        films = years_selector()[0:10]
+        self.assertEqual(films.count(), 4)  # four films in the setUp
+        self.assertEqual(films[0], 2022)
+        # Test with empty db
+        Film.objects.all().delete()
+        films = years_selector()
+        self.assertEqual(films.count(), 0)
 
     def test_related_film_selector(self):
-        result = related_film_selector(self.film)
-        self.assertEqual(len(result), 4)
-        self.assertEqual(result[3].title, 'christmas')
-        self.assertEqual(result[0].release_year, 2002)
-        self.assertEqual(result[2].rating, 6)
-        self.assertEqual(result[1].views, '756')
-        self.assertEqual(result[0].categories.all()[0].name, 'action')
+        # Test with default films_number (4)
+        films = related_film_selector(self.film)
+        self.assertEqual(films.count(), 4)
+        self.assertEqual(films[0].categories.first().name, 'action')
+        self.assertEqual(films[1].categories.first().name, 'comedy')
+        # Test with films_number=2
+        films = related_film_selector(self.film)[1:3]
+        self.assertEqual(films.count(), 2)
+        self.assertEqual(films[0].categories.first().name, 'comedy')
+        self.assertEqual(films[1].categories.first().name, 'melodrama')
+        # Test with films_number > number of films in db
+        films = related_film_selector(self.film)[0:10]  # four films in the setUp
+        self.assertEqual(films.count(), 4)
+        self.assertEqual(films[0].categories.first().name, 'action')
+        self.assertEqual(films[1].categories.first().name, 'comedy')
+        # Test with empty db
+        Film.objects.all().delete()
+        films = related_film_selector(self.film)
+        self.assertEqual(films.count(), 0)
 
     def test_related_director_selector(self):
-        result = related_director_selector(self.film)
-        self.assertEqual(len(result), 2)
-        self.assertEqual(result[0].title, 'avatar')
-        self.assertEqual(result[1].title, 'christmas')
-
-    def test_related_director_selector_fail(self):
-        result = related_director_selector(self.film)
-        with self.assertRaises(IndexError):
-            result[2]
-
-
-
+        # Test with default films_number (4)
+        films = related_director_selector(self.film)
+        self.assertEqual(films.count(), 2)
+        self.assertEqual(films[0].title, 'avatar')
+        self.assertEqual(films[1].title, 'christmas')
+        # Test with films_number=1
+        films = related_director_selector(self.film)[:1]
+        self.assertEqual(films.count(), 1)
+        self.assertEqual(films[0].title, 'avatar')
+        # Test with films_number > number of films in db
+        films = related_director_selector(self.film)[:10]  # two films in the setUp
+        self.assertEqual(films.count(), 2)
+        self.assertEqual(films[0].title, 'avatar')
+        self.assertEqual(films[1].title, 'christmas')
+        # Test with empty db
+        Film.objects.all().delete()
+        films = related_director_selector(self.film)
+        self.assertEqual(films.count(), 0)
